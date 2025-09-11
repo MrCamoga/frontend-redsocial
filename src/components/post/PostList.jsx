02506ec00dbb3358d0f9324d58111de66223d3ba
Carlos Moya <mrcamoga@gmail.com>
2025-09-11 02:25:58 +0200

    -1 line

--- src/components/post/PostList.jsx
+++ src/components/post/PostList.jsx
@@ -1,7 +1,6 @@
 import MiniPost from "./MiniPost"
 
 const PostList = ({posts,user}) => {
-
     return <div className="postList">
         {posts.map(post => <MiniPost key={post._id} post={user ? {...post,userId:user}:post}/>)}
     </div>
